063af2ac0fc67192abe10bee06c3bf2cb607bc9d
twitecki <witeckitom@gmail.com>
2014-11-21 03:15:05 -0600

    again...

--- webapp/clusteringapp/clusteringapp/settings.py
+++ webapp/clusteringapp/clusteringapp/settings.py
@@ -82,5 +82,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-STATIC_ROOT = 'webapp/clusteringapp/dataclustering/static'
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
