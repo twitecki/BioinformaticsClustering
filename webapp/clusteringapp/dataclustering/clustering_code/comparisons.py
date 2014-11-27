5383f0fdf984f5be348cce2b2716b5501516caed
twitecki <witeckitom@gmail.com>
2014-11-27 03:10:43 -0600

    Working for 100 now

--- webapp/clusteringapp/dataclustering/clustering_code/comparisons.py
+++ webapp/clusteringapp/dataclustering/clustering_code/comparisons.py
@@ -9,7 +9,15 @@ def getEuclideanDistance(obj1, obj2, precision):
 	valueArr2 = obj2.values
 	acc = 0
 	for i in range(len(valueArr1)):
-		v1 = float(valueArr1[i])
-		v2 = float(valueArr2[i])
+		v1 = 0
+		v2 = 0
+		try:
+			v1 = float(valueArr1[i]) * 10
+		except ValueError:
+			v1 = 0
+		try:
+			v2 = float(valueArr2[i]) * 10
+		except ValueError:
+			v2 = 0
 		acc += (v1 - v2)**2
 	return round(math.sqrt(acc), precision)
