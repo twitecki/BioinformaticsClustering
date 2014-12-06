ed69a07f770187929c91871c0deae2ff7947597a
twitecki <witeckitom@gmail.com>
2014-12-06 14:16:46 -0600

    Redesigning UI

--- webapp/clusteringapp/dataclustering/clustering_code/run.py
+++ webapp/clusteringapp/dataclustering/clustering_code/run.py
@@ -7,21 +7,25 @@ from testFunctions import *
 from display import *
 from fileWriting import *
 from fileReading import *
+from ClusteringSettings import *
 
 def findAnswer(matrix, nodeArray):
 	nArray = nodeArray
 	while(len(matrix) > 2):
-		printDistanceMatrix(matrix)
-		print nArray
+		if (DEBUG):
+			printDistanceMatrix(matrix)
 		result = mergeClusters(matrix, nArray,False)
 		matrix = result[0]
 		nArray = result[1]
-	printDistanceMatrix(matrix)
-	print nArray
+
+	if (DEBUG):
+		printDistanceMatrix(matrix)
+		print nArray
 
 	result = mergeClusters(matrix, nArray,True)
 	nArray = result[1]
-	print nArray
+	if (DEBUG):
+		print nArray
 
 	return nArray[len(nArray)-1]
 
@@ -55,6 +59,7 @@ def buildJSONTree(inputFile):
 	fileLocation = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '../../clusteringapp/static/dendro.json'))
 	createJSON(fileLocation, root)
 
+
 #da = readDistancesFromFile('TestTextFiles/test1.txt')
 #da = readDistancesFromFile('TestTextFiles/test2.txt')
 def printTree(node):
@@ -72,8 +77,9 @@ try :
 	test = buildTestDistanceMatrix(da)
 	nodeArray = buildInitialNodeArray(test)
 	root = findAnswer(test, nodeArray)
-	print ''
-	printTree(root)
+	if (DEBUG):
+		print ''
+		printTree(root)
 	createJSON(str(sys.argv[2]), root)
 except IOError:
 	print "File not found"
