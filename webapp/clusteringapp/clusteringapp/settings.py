d79ac51ff816436cd41a0862066a82d4ec785910
twitecki <witeckitom@gmail.com>
2014-12-15 06:23:28 -0600

    Debugging

--- webapp/clusteringapp/clusteringapp/settings.py
+++ webapp/clusteringapp/clusteringapp/settings.py
@@ -25,7 +25,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SECRET_KEY = 'p4n#^k)o!fwlu1$@3n0ec_axj%(5*w++j4i*7he$q@m03$80z)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
+
 TEMPLATE_DEBUG = True
 
 #ALLOWED_HOSTS = ['*']
